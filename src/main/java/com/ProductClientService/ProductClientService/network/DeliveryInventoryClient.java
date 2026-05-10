c7c4979e0db0f7d22fb6106c4e726db327a88c6d
DEEPAKGUPTA1141421 <deepak2198.be21@chitkara.edu.in>
2026-05-10 22:05:39 +0530

    Update delivery inventory client and DTO

--- src/main/java/com/ProductClientService/ProductClientService/network/DeliveryInventoryClient.java
+++ src/main/java/com/ProductClientService/ProductClientService/network/DeliveryInventoryClient.java
@@ -2,6 +2,7 @@ package com.ProductClientService.ProductClientService.network;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.ProductClientService.ProductClientService.DTO.ApiResponse;
 import com.ProductClientService.ProductClientService.DTO.network.DeliveryEstimateDto;
 import com.ProductClientService.ProductClientService.DTO.network.DeliveryInvetoryApiDto.CreateRiderDto;
+import com.ProductClientService.ProductClientService.DTO.network.DeliveryInvetoryApiDto.RiderDetailsResponse;
 import com.ProductClientService.ProductClientService.DTO.network.DeliveryInvetoryApiDto.RiderIdResponse;
 
-@FeignClient(name = "rider", url = "${feign.client.delivery_inventory_client.url}")
+@FeignClient(name = "rider", url = "${feign.client.delivery_inventory_client.url}", configuration = DeliveryInventoryFeignConfig.class)
 public interface DeliveryInventoryClient {
 
     @PostMapping("/api/v1/riders/signup")
     ApiResponse<RiderIdResponse> createRiderWithPhone(@RequestBody CreateRiderDto request);
+
+    @GetMapping("/api/v1/riders/{phone}")
+    RiderDetailsResponse getRiderByPhone(@PathVariable("phone") String phone);
 
     /**
      * Calls DeliveryInventoryService to get a 3-segment delivery estimate
